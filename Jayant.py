3f92790a338f6f839decca5b67304c11a370a320
Jasdeep  Singh <82466502+Jassingh6060@users.noreply.github.com>
2021-11-03 22:24:14 +0530

    Update Jayant.py

--- Jayant.py
+++ Jayant.py
@@ -1,19 +1,17 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO  ## GPIO library
+import time   ## Time library
 
-import time
 
-GPIO.setmode(GPIO.BOARD)
+##declaring pins and board
+GPIO.setmode(GPIO.BOARD)  
+GPIO.setup(14,GPIO.OUT)
 
-GPIO.setup(18,GPIO.OUT)
-GPIO.output(18,GPIO.HIGH)
 
-time.sleep(0.25)
-GPIO.output(18,GPIO.LOW)
 try:
     while 1:
-        GPIO.output(18,GPIO.HIGH)
-        time.sleep(0.25)
-        GPIO.output(18,GPIO.LOW)
-        time.sleep(0.25)
+        GPIO.output(14,GPIO.HIGH)   
+        time.sleep(0.5) ## time delay for 0.5 seconds
+        GPIO.output(14,GPIO.LOW)
+        time.sleep(0.5)
 except KeyboardInterrupt:
     GPIO.cleanup()
